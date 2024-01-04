0dfdea9332b3bb671c47f743b3e02cfa84e3cc0f
Maksim Kosenko <maksim.kosenko@rosbank.ru>
2024-01-04 19:31:16 +0500

    Initial Commit

---
+++ FoodAdvisor/FoodAdvisorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAdvisorApp.swift
+//  FoodAdvisor
+//
+//  Created by Максим Косенко on 04.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodAdvisorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
